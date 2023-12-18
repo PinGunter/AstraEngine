7fdc92792fc926432cad3bf275620f305a13126a
Salva Romero <salvaromero08@gmail.com>
2023-12-18 14:50:44 +0100

    image views

--- src/main.cpp
+++ src/main.cpp
@@ -91,6 +91,9 @@ private:
 	std::vector<VkImage> swapchainImages;
 	VkFormat swapchainImageFormat;
 	VkExtent2D swapchainExtent;
+	// image views
+	std::vector<VkImageView> swapchainImageViews;
+
 
 	void initWindow() {
 		glfwInit();
@@ -108,6 +111,7 @@ private:
 		pickPhysicalDevice();
 		createLogicalDevice();
 		createSwapchain();
+		createImageViews();
 	}
 
 	void mainLoop() {
@@ -117,6 +121,10 @@ private:
 	}
 
 	void cleanup() {
+
+		for (auto imageView : swapchainImageViews) {
+			vkDestroyImageView(device, imageView, nullptr);
+		}
 
 		vkDestroySwapchainKHR(device, swapchain, nullptr);
 
@@ -424,6 +432,33 @@ private:
 
 		swapchainImageFormat = surfaceFormat.format;
 		swapchainExtent = extent;
+	}
+
+	void createImageViews() {
+		swapchainImageViews.resize(swapchainImages.size());
+
+		for (size_t i = 0; i < swapchainImages.size(); ++i) {
+			VkImageViewCreateInfo createInfo{};
+			createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			createInfo.image = swapchainImages[i];
+			createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D; // or 1D, 3D, cube maps, etc...
+			createInfo.format = swapchainImageFormat;
+			// swizzle the color around, maybe map all channels to red for monochrome, for example.
+			createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+			createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			createInfo.subresourceRange.baseMipLevel = 0;
+			createInfo.subresourceRange.levelCount = 1;
+			createInfo.subresourceRange.baseArrayLayer = 0;
+			createInfo.subresourceRange.layerCount = 1;
+
+			if (vkCreateImageView(device, &createInfo, nullptr, &swapchainImageViews[i]) != VK_SUCCESS) {
+				throw std::runtime_error("failed to create image views");
+			}
+		}
 	}
 
 	void createLogicalDevice() {
