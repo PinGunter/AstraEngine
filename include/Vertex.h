2a9559fe5bff927cd0b32355827707c73d056d39
PinGunter <salvaromero08@gmail.com>
2024-01-09 18:58:52 +0100

    textures working

--- include/Vertex.h
+++ include/Vertex.h
@@ -9,6 +9,7 @@
 struct Vertex {
 	glm::vec3 pos;
 	glm::vec3 color;
+	glm::vec2 texCoord;
 
 	static vk::VertexInputBindingDescription getBindingDescription() {
 		return vk::VertexInputBindingDescription(
@@ -18,12 +19,13 @@ struct Vertex {
 		);
 	}
 
-	static std::array<vk::VertexInputAttributeDescription, 2> getAttributeDescriptions() {
+	static std::array<vk::VertexInputAttributeDescription, 3> getAttributeDescriptions() {
 		// arguments in order:
 		// location, binding, format, offset
 		return { {
 			vk::VertexInputAttributeDescription(0, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, pos)),
-			vk::VertexInputAttributeDescription(1, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, color))
+			vk::VertexInputAttributeDescription(1, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, color)),
+			vk::VertexInputAttributeDescription(2, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, texCoord))
 			} };
 	}
 };
