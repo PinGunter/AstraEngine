a2076431b113395f47585dd41d1f88bf601b8b10
Salva Romero <salvaromero08@gmail.com>
2024-01-11 01:49:36 +0100

    updated .gitignore

--- include/VulkanApp.h
+++ include/VulkanApp.h
@@ -17,8 +17,8 @@ const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 const int MAX_FRAMES_IN_FLIGHT = 2;
 
-const std::string MODEL_PATH = "models/viking_room.obj";
-const std::string TEXTURE_PATH = "textures/viking_room.png";
+const std::string MODEL_PATH = "models/spot.obj";
+const std::string TEXTURE_PATH = "textures/spot.png";
 
 
 const std::vector<const char*> validationLayers = {
