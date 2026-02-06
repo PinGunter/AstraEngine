00cfdebe3337db2952adfcd02b37561ef00b423c
Salva Romero <salvaromero08@gmail.com>
2026-02-06 11:06:55 +0100

    start work with descriptors

--- src/vk_descriptors.cpp
+++ src/vk_descriptors.cpp
@@ -1,1 +1,52 @@
 ﻿#include <vk_descriptors.h>
+
+void DescriptorLayoutBuilder::add_binding(uint32_t binding, VkDescriptorType type)
+{
+	VkDescriptorSetLayoutBinding newBind{};
+	newBind.binding = binding;
+	newBind.descriptorCount = 1;
+	newBind.descriptorType = type;
+
+	bindings.push_back(newBind);
+}
+
+void DescriptorLayoutBuilder::clear()
+{
+	bindings.clear();
+}
+
+VkDescriptorSetLayout DescriptorLayoutBuilder::build(VkDevice device, VkShaderStageFlags shaderStages, void* pNext, VkDescriptorSetLayoutCreateFlags flags)
+{
+	for (auto& b : bindings) {
+		b.stageFlags |= shaderStages;
+	}
+
+	VkDescriptorSetLayoutCreateInfo info = { .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO };
+	info.pNext = pNext;
+
+	info.pBindings = bindings.data();
+	info.bindingCount = (uint32_t)bindings.size();
+	info.flags = flags;
+
+	VkDescriptorSetLayout set;
+	VK_CHECK(vkCreateDescriptorSetLayout(device, &info, nullptr, &set));
+
+	return set;
+}
+
+void DescriptorAllocator::init_pool(VkDevice device, uint32_t maxSets, std::span<PoolSizeRatio> poolRatios)
+{
+}
+
+void DescriptorAllocator::clear_descriptors(VkDevice device)
+{
+}
+
+void DescriptorAllocator::destroy_pool(VkDevice device)
+{
+}
+
+VkDescriptorSet DescriptorAllocator::allocate(VkDevice device, VkDescriptorSetLayout layout)
+{
+	return VkDescriptorSet();
+}
